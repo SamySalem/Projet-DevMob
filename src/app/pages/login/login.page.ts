d92fab8fdf725f944f3e6bc5d77f267234824a2e
Sami Salem <sami.salem@etu.univ-grenoble-alpes.fr>
2021-03-24 00:21:44 +0100

    Revert "ajout methode google auth"
    This reverts commit 76da851082c12b5d39edfcf9c448f47f2421c314.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -26,23 +26,16 @@ export class LoginPage {
     });
   }
 
-  /* async googleSignIn(){
-        const googleUser = await Plugins.GoogleAuth.signIn();
-        const credential = firebase.auth.GoogleAuthProvider.credential(googleUser.authentication.idToken);
-        await this.afAuth.signInAndRetrieveDataWithCredential(credential);
-        this.router.navigateByUrl('/');
-  } */
-
 
   async loginForm(){
 
     if (this.login.valid) {
-
+      
       try {
-        await this.auth.login(this.login.get('email').value,
+        await this.auth.login(this.login.get('email').value, 
         this.login.get('password').value);
         this.route.navigate(['home']);
-
+        
       } catch (e) {
         const toast = await this.toastController.create({
           color: 'danger',
